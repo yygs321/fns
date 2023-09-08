547a0641a170c2d064ec537d359aa44e7b55634a
sgkim <kimsg64090@gmail.com>
2023-09-08 10:39:43 +0900

    first commit

---
+++ backend/src/main/java/ssafy/fns/FnsApplication.java
@@ -0,0 +1,13 @@
+package ssafy.fns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FnsApplication.class, args);
+    }
+
+}
