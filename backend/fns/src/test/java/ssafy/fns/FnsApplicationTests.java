4daf454243cf2904ca3e6efdb09a623b28e03629
sgkim <kimsg64090@gmail.com>
2023-09-08 12:51:48 +0900

    second commit

---
+++ backend/fns/src/test/java/ssafy/fns/FnsApplicationTests.java
@@ -0,0 +1,13 @@
+package ssafy.fns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
