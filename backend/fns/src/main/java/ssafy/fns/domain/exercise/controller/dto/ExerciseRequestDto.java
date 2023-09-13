f48d216f63b1e9fd7fba43a820ea6fb3d083d2e6
박소민 <yygs321@naver.com>
2023-09-13 13:40:08 +0900

    [BE/feat] 빈 Dto 파일에 생성자 어노테이션 붙여서 발생한 에러 수정

--- backend/fns/src/main/java/ssafy/fns/domain/exercise/controller/dto/ExerciseRequestDto.java
+++ backend/fns/src/main/java/ssafy/fns/domain/exercise/controller/dto/ExerciseRequestDto.java
@@ -1,12 +1,8 @@
 package ssafy.fns.domain.exercise.controller.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ExerciseRequestDto {
 
 }
