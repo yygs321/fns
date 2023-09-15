50c07121f05c198451fb7f1205715892218de7dd
박소민 <yygs321@naver.com>
2023-09-15 17:06:33 +0900

    [BE/feat] controller에 signUp 메서드 추가

--- backend/fns/src/main/java/ssafy/fns/domain/member/controller/MemberController.java
+++ backend/fns/src/main/java/ssafy/fns/domain/member/controller/MemberController.java
@@ -1,16 +1,28 @@
 package ssafy.fns.domain.member.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ssafy.fns.domain.member.controller.dto.SignUpRequestDto;
+import ssafy.fns.domain.member.service.MemberService;
+import ssafy.fns.global.response.JsonResponse;
 
 @RestController
 @RequestMapping(value = "/api/members")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping(value = "/sign-up")
+    public ResponseEntity<?> signUp(@RequestBody SignUpRequestDto requestDto) {
+        memberService.signUp(requestDto);
+        return JsonResponse.ok("회원가입 성공!");
+    }
 
 }
