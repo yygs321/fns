8704e9b43d8f4de751c6371df48a0a09932e7800
hyunin3 <hjshimel@gmail.com>
2023-09-15 13:00:08 +0900

    [FE/feat] 음식 검색 페이지 검색방식 및 필터링 변경

--- frontend/React/src/Pages/SearchFoodPage/SearchFoodPage.jsx
+++ frontend/React/src/Pages/SearchFoodPage/SearchFoodPage.jsx
@@ -3,6 +3,8 @@ import {
   Typography, 
   TextField, 
   Grid, 
+  Chip,
+  Divider
   
 } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
@@ -28,96 +30,140 @@ const SearchFoodPage = () => {
     item.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const filters = { kcal, carbs, protein, fat };
+  const activeFilters = Object.keys(filters).filter(key => filters[key]);
+
   return (
     <div style={{ padding: '20px' }}>
       <Typography variant="h4" gutterBottom align="center" fontWeight={"bold"}>
         음식 정보
       </Typography>
-
+  
       <TextField
         fullWidth
         variant="outlined"
         placeholder="음식을 검색하세요"
-        onChange={e => setSearchTerm(e.target.value)}
+        onChange={e => {
+          setSearchTerm(e.target.value);
+          if (!e.target.value) {
+            setKcal('');
+            setCarbs('');
+            setProtein('');
+            setFat('');
+          }
+        }}
         InputProps={{
           endAdornment: <SearchIcon color="primary" />
         }}
       />
-
+  
       <br />
       <br />
-
-      <Grid container direction="column" spacing={3}>
-        <Grid item container alignItems="center" spacing={1}>
-          <Grid item xs={8}>
-          <TextField
-              fullWidth
-              label="Kcal"
-              variant="outlined"
-              type="number"
-              value={kcal}
-              onChange={e => setKcal(e.target.value)}
-            />
+  
+      {searchTerm ? (
+        <div>
+          {activeFilters.length > 0 && (
+            <Grid container spacing={1} style={{ marginBottom: '10px' }}>
+              {activeFilters.map(filterKey => (
+                <Grid item key={filterKey}>
+                  <Chip
+                    label={`${filterKey.toUpperCase()}: ${filters[filterKey]}`}
+                    onDelete={() => {
+                      switch (filterKey) {
+                        case 'kcal':
+                          setKcal('');
+                          break;
+                        case 'carbs':
+                          setCarbs('');
+                          break;
+                        case 'protein':
+                          setProtein('');
+                          break;
+                        case 'fat':
+                          setFat('');
+                          break;
+                        default:
+                          break;
+                      }
+                    }}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          )}
+          <Divider />
+          <FilteredSearchResults data={filteredData} filters={filters} />
+        </div>
+      ) : (
+        <Grid container direction="column" spacing={3}>
+          <Grid item container alignItems="center" spacing={1}>
+            <Grid item xs={8}>
+              <TextField
+                fullWidth
+                label="Kcal"
+                variant="outlined"
+                type="number"
+                value={kcal}
+                onChange={e => setKcal(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <Typography>이하</Typography>
+            </Grid>
           </Grid>
-          <Grid item xs={3}>
-            <Typography>이하</Typography>
+  
+          <Grid item container alignItems="center" spacing={1}>
+            <Grid item xs={8}>
+              <TextField
+                fullWidth
+                label="탄수화물"
+                variant="outlined"
+                type="number"
+                value={carbs}
+                onChange={e => setCarbs(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <Typography>이하</Typography>
+            </Grid>
+          </Grid>
+  
+          <Grid item container alignItems="center" spacing={1}>
+            <Grid item xs={8}>
+              <TextField
+                fullWidth
+                label="단백질"
+                variant="outlined"
+                type="number"
+                value={protein}
+                onChange={e => setProtein(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <Typography>이하</Typography>
+            </Grid>
+          </Grid>
+  
+          <Grid item container alignItems="center" spacing={1}>
+            <Grid item xs={8}>
+              <TextField
+                fullWidth
+                label="지방"
+                variant="outlined"
+                type="number"
+                value={fat}
+                onChange={e => setFat(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <Typography>이하</Typography>
+            </Grid>
           </Grid>
         </Grid>
-
-        <Grid item container alignItems="center" spacing={1}>
-          <Grid item xs={8}>
-          <TextField
-              fullWidth
-              label="탄수화물"
-              variant="outlined"
-              type="number"
-              value={carbs}
-              onChange={e => setCarbs(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={3}>
-            <Typography>이하</Typography>
-          </Grid>
-        </Grid>
-
-        <Grid item container alignItems="center" spacing={1}>
-          <Grid item xs={8}>
-          <TextField
-              fullWidth
-              label="단백질"
-              variant="outlined"
-              type="number"
-              value={protein}
-              onChange={e => setProtein(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={3}>
-            <Typography>이하</Typography>
-          </Grid>
-        </Grid>
-
-        <Grid item container alignItems="center" spacing={1}>
-          <Grid item xs={8}>
-          <TextField
-              fullWidth
-              label="지방"
-              variant="outlined"
-              type="number"
-              value={fat}
-              onChange={e => setFat(e.target.value)}
-            />
-          </Grid>
-          <Grid item xs={3}>
-            <Typography>이하</Typography>
-          </Grid>
-        </Grid>
-      </Grid>
-      <FilteredSearchResults 
-        data={filteredData} 
-        filters={{ kcal, carbs, protein, fat }} 
-      />
+      )}
     </div>
   );
+  
 };
 
 export default SearchFoodPage;
