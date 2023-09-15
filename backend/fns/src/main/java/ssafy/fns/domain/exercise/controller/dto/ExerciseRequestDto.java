c4a30319c6aea6c220483de0d0fcb94fda5f78d3
Park JiHwan <mycook3@naver.com>
2023-09-15 14:53:16 +0900

    DTO 추가 중

--- backend/fns/src/main/java/ssafy/fns/domain/exercise/controller/dto/ExerciseRequestDto.java
+++ backend/fns/src/main/java/ssafy/fns/domain/exercise/controller/dto/ExerciseRequestDto.java
@@ -1,12 +1,20 @@
 package ssafy.fns.domain.exercise.controller.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /*
 * 운동한 칼로리 저장 요청
 * */
 
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ExerciseRequestDto {
     private Long exercise_id;
 
--- backend/fns/src/main/java/ssafy/fns/domain/food/controller/dto/IntakeInsertRequestDto.java
+++ backend/fns/src/main/java/ssafy/fns/domain/food/controller/dto/IntakeInsertRequestDto.java
@@ -2,13 +2,22 @@ package ssafy.fns.domain.food.controller.dto;
 
 import java.util.Date;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ssafy.fns.domain.food.entity.Time;
 
+/*
+* 먹은 거 입력
+* */
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class IntakeInsertRequestDto {
-    private Long intake_id;
+    private Long food_id;
     private Date date;
     private Long rate;
     private Time intake_time;
