9a61693be9c4b657dddf60c04201b06611968ec2
박소민 <yygs321@naver.com>
2023-09-19 15:06:33 +0900

    [BE/feat] 소셜로그인 구조 생성

---
+++ backend/fns/src/main/java/ssafy/fns/domain/auth/service/dto/OAuthLoginResponseDto.java
@@ -0,0 +1,19 @@
+package ssafy.fns.domain.auth.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OAuthLoginResponseDto {
+
+    private OAuthDetailDto detailDto;
+    private TokenResponseDto tokenResponseDto;
+    private String socialLoginType;
+}
