e45a82d781c63e4f4a51770c02920daff2e0e7bf
hyunin3 <hjshimel@gmail.com>
2023-09-20 10:39:54 +0900

    [FE/feat] 콜백페이지 테스트

--- frontend/React/src/Pages/LoginPage/CallbackPage.jsx
+++ frontend/React/src/Pages/LoginPage/CallbackPage.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-// import { loginSaveUserNo } from "../actions/actions"; 필요하다면 userID로 변경 
+// import { useDispatch } from "react-redux";
+// import { loginSaveUserNo } from "../actions/actions"; 필요하다면 member-id로 변경 
 
 const KakaoCallback = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
   const SERVER_API_URL = `${process.env.REACT_APP_API_SERVER_URL}`;
 
   const navigate = useNavigate();
@@ -18,29 +18,34 @@ const KakaoCallback = () => {
 
   const kakaoLogin = (code) => {
     axios
-      .get(`${SERVER_API_URL}/user/login?code=${code}`)
+      .post(`${SERVER_API_URL}/auth/kakao`, { code: code })
       .then((res) => {
-        const userNo = res.data.data;
+        // 어떻게 나오는지 찍어보고 합시다
+          console.log('#1')
+          console.log(res)
+          console.log('#2')
+          console.log(res.data)
+          console.log('#3')
+          console.log(res.message)
 
         // userNo를 비교용으로 사용하기 위해 리덕스 저장
         // dispatch(loginSaveUserNo(userNo));
 
-        if (res.data.code === "201") {
-          // 정보입력 페이지로 이동. 코드는 메시지로 변경
+        if (res.message === "signup") {
+          // 메시지에 따라서 회원가입이면 정보입력 페이지로 이동. 
           navigate(`/Info`);
-        } else if (res.data.code === "200") {
+        } else if (res.message === "login") {
+          // 메시지에 따라서 로그인이면 메인 페이지로 이동. 
           localStorage.setItem(
             "access-token",
-            res.headers.get("authorization")
+            res.data.accessToken
           );
           localStorage.setItem(
             "refresh-token",
-            res.headers.get("refresh-token")
+            res.data.refreshToken
           );
           navigate(`/main`);
-        } else {
-          alert(res.data.message);
-        }
+        } 
       })
       .catch((e) => {
         console.log(e);
