85698e0da84064220e9592eb473df6503831ce3b
hyunin3 <hjshimel@gmail.com>
2023-09-20 13:11:35 +0900

    [FE/fix] 인가코드 확인용

--- frontend/React/src/Pages/LoginPage/CallbackPage.jsx
+++ frontend/React/src/Pages/LoginPage/CallbackPage.jsx
@@ -14,9 +14,11 @@ const KakaoCallback = () => {
     const params = new URL(window.location.href).searchParams;
     const code = params.get("code");
     kakaoLogin(code);
+    console.log(code)
   });
 
   const kakaoLogin = (code) => {
+    console.log(code)
     axios
       .post(`${SERVER_API_URL}/auth/kakao`, { code: code })
       .then((res) => {
