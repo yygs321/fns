81b5a94e52c42d08d6d6f49866f1f7ff9fa23ea6
Park JiHwan <mycook3@naver.com>
2023-09-21 15:22:29 +0900

    [BE/ENV] Swagger AccessToken 값 입력창  추가

--- backend/fns/src/main/java/ssafy/fns/global/config/SwaggerConfiguration.java
+++ backend/fns/src/main/java/ssafy/fns/global/config/SwaggerConfiguration.java
@@ -1,22 +1,25 @@
 package ssafy.fns.global.config;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @EnableWebMvc
-public class SwaggerConfiguration {
+public class SwaggerConfiguration extends WebMvcConfigurerAdapter {
 
     /**
-     * http://localhost:8081/swagger-ui/
+     * http://localhost:8081/api/swagger-ui/
      */
 
     @Bean
@@ -27,8 +30,13 @@ public class SwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("ssafy.fns"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(Arrays.asList(apiKey()));
 
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("accessToken", "X-FNS-ACCESSTOKEN", "header");
     }
 
     @Bean
