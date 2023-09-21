736149b8b6928a654d4c2dd9f0c1b3b03c7ddfc2
Park JiHwan <mycook3@naver.com>
2023-09-21 17:55:33 +0900

    [BE/ENV] Swagger에 AccessToken 입력하는 창 추가

--- backend/fns/src/main/java/ssafy/fns/global/config/SwaggerConfiguration.java
+++ backend/fns/src/main/java/ssafy/fns/global/config/SwaggerConfiguration.java
@@ -1,22 +1,25 @@
 package ssafy.fns.global.config;
 
 import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-@EnableWebMvc
-public class SwaggerConfiguration extends WebMvcConfigurerAdapter {
+@RequiredArgsConstructor
+public class SwaggerConfiguration {
 
     /**
      * http://localhost:8081/api/swagger-ui/
@@ -25,18 +28,30 @@ public class SwaggerConfiguration extends WebMvcConfigurerAdapter {
     @Bean
     public Docket swaggerAPI(){
         return new Docket(DocumentationType.OAS_30)
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .useDefaultResponseMessages(true)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("ssafy.fns"))
                 .paths(PathSelectors.any())
-                .build()
-                .securitySchemes(Arrays.asList(apiKey()));
+                .build();
 
     }
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
 
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("X-FNS-ACCESSTOKEN", authorizationScopes));
+    }
     private ApiKey apiKey() {
-        return new ApiKey("accessToken", "X-FNS-ACCESSTOKEN", "header");
+        return new ApiKey("X-FNS-ACCESSTOKEN", "X-FNS-ACCESSTOKEN", "header");
     }
 
     @Bean
@@ -51,5 +66,4 @@ public class SwaggerConfiguration extends WebMvcConfigurerAdapter {
                 .version("2.0")
                 .build();
     }
-
 }
