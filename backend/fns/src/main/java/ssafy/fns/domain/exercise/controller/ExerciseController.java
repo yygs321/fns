d3bd43b372e46fafd5e711578e1ac9e245be7a5d
박소민 <yygs321@naver.com>
2023-09-22 17:52:09 +0900

    [BE/feat] 운동 기록 추가 구조 생성

--- backend/fns/src/main/java/ssafy/fns/domain/exercise/controller/ExerciseController.java
+++ backend/fns/src/main/java/ssafy/fns/domain/exercise/controller/ExerciseController.java
@@ -5,13 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ssafy.fns.domain.exercise.controller.dto.ExerciseRequestDto;
 import ssafy.fns.domain.exercise.service.ExerciseService;
+import ssafy.fns.domain.member.entity.Member;
 import ssafy.fns.global.response.JsonResponse;
 
 @RestController
@@ -23,4 +23,10 @@ public class ExerciseController {
 
     private final ExerciseService exerciseService;
 
+    @PostMapping
+    public ResponseEntity<?> saveExercise(@AuthenticationPrincipal Member member,
+            @RequestBody ExerciseRequestDto requestDto) {
+        exerciseService.saveExercise(member, requestDto);
+        return JsonResponse.ok("운동 저장!");
+    }
 }
