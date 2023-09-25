9759c502680e9eb5249fb2a204a7b44aacbfc4b3
sgkim <kimsg64090@gmail.com>
2023-09-25 14:52:42 +0900

    [BE/feat] MySQL 변수 숨김

--- recommend/app.py
+++ recommend/app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from database import engineconn
 from sqlalchemy import Table, MetaData
+from database import engineconn
 import redis
 
 app = FastAPI()
--- recommend/database.py
+++ recommend/database.py
@@ -4,10 +4,11 @@ from settings import DATABASE_USER, DATABASE_PASSWORD
 
 DB_URL = f'mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@http://13.124.188.144:3306/fns_database'
 
+
 class engineconn:
 
     def __init__(self):
-        self.engine = create_engine(DB_URL, pool_recycle = 500)
+        self.engine = create_engine(DB_URL, pool_recycle=500)
 
     def sessionmaker(self):
         Session = sessionmaker(bind=self.engine)
