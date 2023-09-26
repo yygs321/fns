099f2a7de9226635c85bdcc52a6a35b9ccb99c1d
sgkim <kimsg64090@gmail.com>
2023-09-26 15:15:40 +0900

    [BE/feat] recommend_food 테스트

--- recommend/app.py
+++ recommend/app.py
@@ -4,6 +4,8 @@ from sqlalchemy import Table, MetaData
 from recommend.database import engineconn
 import redis
 import sys
+import numpy as np
+import math
 
 print(sys.path)
 
@@ -34,7 +36,11 @@ async def load_food_data_to_redis():
     foods = db.query(Food).all()
 
     for food in foods:
-        redis_db.set(str(food.food_id), str(food))
+        food_id = str(food.food_id)
+        for column in Food.__table__.columns:
+            key = column.name
+            value = getattr(food, key)
+            redis_db.hset(food_id, key, value)
 
     print("Data loaded to Redis at startup!")
 
@@ -42,5 +48,37 @@ async def load_food_data_to_redis():
 @app.post("/fastapi/recommend")
 async def test(offset: Offset):
     print(offset)
-    return {"offset": offset}
+    user_diffs = (offset.calorie, offset.carbohydrate, offset.protein)
 
+    food_keys = redis_db.keys("food:*")
+    foods_data = {}
+
+    for key in food_keys:
+        foods_data[key.decode()] = redis_db.hgetall(key)
+
+    for food_key, data in foods_data.items():
+        print(food_key, {k.decode(): v.decode() for k, v in data.items()})
+    #
+    # weights = []
+    # for food in foods:
+    #
+    #
+    # return {"offset": offset}
+    return {"foods": foods_data}
+
+
+def recommend_food(calorie_diff, carb_diff, protein_diff, w_i=[1, 1, 1], λ=1):
+    # 각 영양소의 차이 계산
+    diffs = [
+        math.log(1 + abs(calorie_diff)),
+        math.log(1 + abs(carb_diff)),
+        math.log(1 + abs(protein_diff))
+    ]
+
+    # 균형 패널티 계산
+    balance_penalty = np.std(diffs)
+
+    # 전체 가중치 합 계산
+    W = sum([w * diff for w, diff in zip(w_i, diffs)]) + λ * balance_penalty
+
+    return W
