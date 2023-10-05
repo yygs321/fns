dc894b81ef1953d2131f3b886fc7dcd10da2bda3
박소민 <yygs321@naver.com>
2023-10-05 18:19:21 +0900

    [BE/fix] 운동 조회 쿼리 수정

--- backend/fns/src/main/java/ssafy/fns/domain/exercise/repository/ExerciseRepository.java
+++ backend/fns/src/main/java/ssafy/fns/domain/exercise/repository/ExerciseRepository.java
@@ -7,13 +7,13 @@ import ssafy.fns.domain.exercise.entity.Exercise;
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
     @Query(nativeQuery = true,
-            value = "SELECT * FROM Exercise e WHERE e.member_id = :memberId AND e.sports_id=:sportsId AND DATE_FORMAT(e.exercise_date, '%Y-%m-%d') LIKE CONCAT(:exerciseDate, '%') ORDER BY e.created_At DESC limit 1")
+            value = "SELECT * FROM exercise e WHERE e.member_id = :memberId AND e.sports_id=:sportsId AND DATE_FORMAT(e.exercise_date, '%Y-%m-%d') LIKE CONCAT(:exerciseDate, '%') ORDER BY e.created_At DESC limit 1")
     Exercise findTop1ByExerciseDateAndMember_IdAndSports_IdOrderByIdDesc(String exerciseDate,
             Long memberId,
             Long sportsId);
 
     @Query(nativeQuery = true,
-            value = "SELECT * FROM Exercise e WHERE e.member_id = :memberId AND DATE_FORMAT(e.exercise_date, '%Y-%m-%d') LIKE CONCAT(:exerciseDate, '%') ORDER BY e.created_At DESC limit 1")
+            value = "SELECT * FROM exercise e WHERE e.member_id = :memberId AND DATE_FORMAT(e.exercise_date, '%Y-%m-%d') LIKE CONCAT(:exerciseDate, '%') ORDER BY e.created_At DESC limit 1")
     Exercise findFirstByExerciseDateAndMember_Id(String exerciseDate, Long memberId);
 
 }
