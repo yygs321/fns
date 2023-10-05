c12d7bfb3e27116a2368e6587d3d5d241b263e2d
ChrisKim <chriskim9512@gmail.com>
2023-10-05 16:38:29 +0900

    [FE/feat] 캘린더 페이지 API URL 수정

--- frontend/React/src/Pages/CalendarPage/CalendarPage.jsx
+++ frontend/React/src/Pages/CalendarPage/CalendarPage.jsx
@@ -64,7 +64,7 @@ const CalendarPage = () => {
 
   useEffect(() => {
 
-  axios.post(`${SERVER_API_URL}/weight/history`, {
+  axios.post(`${SERVER_API_URL}/exercise`, {
     exerciseDate: "2021-10-04",
       headers: {
         "X-FNS-ACCESSTOKEN": accessToken,
