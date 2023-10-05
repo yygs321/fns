08ae19e7d0ae74c054ebd52a83eee2dc196a55fb
hyunin3 <stock4share2@gmail.com>
2023-10-05 23:32:47 +0900

    [FE/feat]달력에서 데이터 한번에 불러오고 불러온 값 콘솔로그로 확인

--- frontend/React/src/Pages/CalendarPage/CalendarPage.jsx
+++ frontend/React/src/Pages/CalendarPage/CalendarPage.jsx
@@ -189,6 +189,7 @@ const CalendarPage = () => {
       let newCalendarData = {};
       responses.forEach((response, index) => {
         if (response.data.success) {
+          console.log("#1", response.data);
           const month = months[index];
           newCalendarData = {
             ...newCalendarData,
@@ -202,7 +203,7 @@ const CalendarPage = () => {
         }
       });
       setCalendarData(newCalendarData);
-      console.log("#1", response.data);
+      
       console.log("Fetched Calendar Data:", newCalendarData);
     }).catch(error => {
       console.error("Data fetching error:", error);
